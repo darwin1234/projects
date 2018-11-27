7ebaa57819f752212715eaf381db9b22c823c2ba
justin domingo <nitsujers@gmail.com>
2018-11-27 08:00:12 +0800

    Category database

---
+++ china-directory/db/category.sql
@@ -0,0 +1,19 @@
+/*
+SQLyog Community v13.0.0 (64 bit)
+MySQL - 10.1.21-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `category` (
+	`category_id` int (11),
+	`business_category` varchar (765)
+); 
+insert into `category` (`category_id`, `business_category`) values('3','Beauty Salon');
+insert into `category` (`category_id`, `business_category`) values('1','Dentistry');
+insert into `category` (`category_id`, `business_category`) values('7','Fishing');
+insert into `category` (`category_id`, `business_category`) values('2','Flower Shop');
+insert into `category` (`category_id`, `business_category`) values('6','Food');
+insert into `category` (`category_id`, `business_category`) values('8','Games');
+insert into `category` (`category_id`, `business_category`) values('4','Hiking');
+insert into `category` (`category_id`, `business_category`) values('5','Laundry');
