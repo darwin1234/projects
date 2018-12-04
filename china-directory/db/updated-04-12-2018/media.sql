843399e65eb152b5d6df460ceb8f0b8340818e86
darwin1234 <darwinsese@gmail.com>
2018-12-04 20:52:04 +0800

    new database

---
+++ china-directory/db/updated-04-12-2018/media.sql
@@ -0,0 +1,29 @@
+/*
+SQLyog Trial v13.0.1 (64 bit)
+MySQL - 10.1.24-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `media` (
+	`image_id` int (50),
+	`id_no` int (11),
+	`image_name` varchar (2295),
+	`image_path` varchar (2295),
+	`publish_date` date 
+); 
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('1','263','seven-stars-resort-spa.jpg','http://192.168.0.199/2018/11/263/images/seven-stars-resort-spa.jpg','2018-11-22');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('14','263','sanrst-omni-la-costa-resort-aerial-pool-1.jpg','http://192.168.0.199/2018/11/263/images/sanrst-omni-la-costa-resort-aerial-pool-1.jpg','2018-11-22');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('15','263','tmg-facebook_social.jpg','http://192.168.0.199/2018/11/263/images/tmg-facebook_social.jpg','2018-11-22');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('16','263','view.jpg','http://192.168.0.199/2018/11/263/images/view.jpg','2018-11-22');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('17','263','welcome-in-paradise-on.jpg','http://192.168.0.199/2018/11/263/images/welcome-in-paradise-on.jpg','2018-11-22');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('18','264','makaibowls.jpg','http://192.168.0.199/2018/11/263/images/makaibowls.jpg','2018-12-03');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('19','264','flotsamjetsam.jpg','http://192.168.0.199/2018/11/263/images/flotsamjetsam.jpg','2018-12-03');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('20','264','gotome.jpg','http://192.168.0.199/2018/11/263/images/gotome.jpg','2018-12-03');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('21','264','kahuna.jpg','http://192.168.0.199/2018/11/263/images/kahuna.jpg','2018-12-03');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('22','264','nisceskinandface.jpg','http://192.168.0.199/2018/11/263/images/nisceskinandface.jpg','2018-12-03');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('23','264','surfshack.jpg','http://192.168.0.199/2018/11/263/images/surfshack.jpg','2018-12-03');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('24','264','bobby-barber.jpg','http://192.168.0.199/2018/11/263/images/bobby-barbers.jpg','2018-12-03');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('25','329','coffeelibrary.jpg','http://192.168.0.199/2018/11/263/images/coffeelibrary.jpg','2018-12-04');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('26','329','jollibee.jpg','http://192.168.0.199/2018/11/263/images/jollibee.jpg','2018-12-04');
+insert into `media` (`image_id`, `id_no`, `image_name`, `image_path`, `publish_date`) values('27','329','thefoodproject.jpg','http://192.168.0.199/2018/11/263/images/thefoodproject.jpg','2018-12-04');
